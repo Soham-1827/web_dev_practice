05c5b02beebf5feed8cafd28a0839caa6f6ec994
Soham-1827 <sohamchoulwar.27@gmail.com>
2025-05-21 09:36:54 +0530

    Project Files

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -2,48 +2,73 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+const app = express();
+const port = 3000;
 
-const client = new pg.Client({
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
   password: "Choulwar1827",
   port: 5433,
-})
-
-client.connect();
-
-
-const app = express();
-const port = 3000;
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
-  const result = await client.query("SELECT country_code FROM visited_countries");
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
-  for(let row of result.rows){
-    countries.push(row.country_code);
-  }
-  console.log(result.rows);
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
-  client.end();
 });
 
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
 
-app.post("/add", async (req, res)=>{
-  const country = req.body.country;
-  const countryCode = client.query("SELECT country_code FROM countries WHERE country_name = $1", [country]);
-  if((await countryCode).rows.length !== 0){
-    const code = (await countryCode).rows[0].country_code;
-    await client.query('INSERT INTO visited_countries (country_code) VALUES ($1))', [code]);
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
 
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
-  res.redirect("/")
 });
-  
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
